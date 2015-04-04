cdc73fc0dcc57952e37f2e0cc3f1abbb90a21a5c
tywallace <tylerwallace@gmail.com>
2015-04-04 10:30:25 -0400

    Changed 2nd instance deliver_now to deliver in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,7 @@ class User < ActiveRecord::Base
 
   # Sends activation email.
   def send_activation_email
-    UserMailer.account_activation(self).deliver_now
+    UserMailer.account_activation(self).deliver
   end
 
   # Sets the password reset attributes.
