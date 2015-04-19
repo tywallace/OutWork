bef8911919f40c769f168e450c4175bca9cba59b
Justin Walters <jw3043@columbia.edu>
2015-04-19 01:35:42 -0400

    Display work/break status

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -5,6 +5,7 @@ class LogsController < ApplicationController
 		is_completed = params[:log][:result] unless params[:log].nil?
 
 		if is_completed == "true"
+			# TODO: Log.new also called in Timer controller
 			@log = Log.new(log_create_params)
 			if @log.save
 				current_user.logs << @log
@@ -12,14 +13,14 @@ class LogsController < ApplicationController
 			else # save failed
 				redirect_to :back
 			end
-		else # pomodoro not completed
+		else # pomodoro not completed or nil
 			redirect_to :back
 		end
 	end
 
 	private
 	def log_create_params
-		params.require(:log)
+		params.require(:log).permit(:result)
 	end
 	def log_update_result_params
 		params.require(:log)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418230909) do
+ActiveRecord::Schema.define(version: 20150419040205) do
 
   create_table "entries", force: true do |t|
     t.string   "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150418230909) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "goal"
+    t.boolean  "result"
   end
 
   add_index "logs", ["user_id"], name: "index_logs_on_user_id"
