bd75bfd9ae52f7b27a0bd1077acaba2d51a7cc13
tywallace <tylerwallace@gmail.com>
2015-04-23 15:30:27 -0400

    Cleaned Up Timer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '870290416342991', '7430ad200fe8fe61b01f3e3a556ed823', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+	#Production
+#  provider :facebook, '870290416342991', '7430ad200fe8fe61b01f3e3a556ed823', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  	
+  	#Development
+  provider :facebook, '883611521677547', '299b9165fe57abd1b37599c5f5dc07e7', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
