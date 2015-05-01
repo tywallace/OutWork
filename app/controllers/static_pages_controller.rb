5cdb2ddc310a2ff7052851c5cbbaf37a3ed9090e
tywallace <tylerwallace@gmail.com>
2015-05-01 01:41:38 -0400

    Created Local Resources for Timer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,10 +5,11 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
-      # @log = Log.new
       @logs = current_user.logs.order("created_at DESC").where("result IS NOT NULL").all
       @goal = current_user.goals.build
       @users = current_user.scoreboard
+      # @log = Log.new
+      # @log.result = true;
       
     end
   end
